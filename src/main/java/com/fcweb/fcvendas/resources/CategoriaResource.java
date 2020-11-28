7d9bb883e097218601d076e98f529a28e4bc9774
flaviofcweb <flaviofcweb@gmail.com>
2020-11-28 16:51:19 -0300

    Testando a primeira classe de dominio

--- src/main/java/com/fcweb/fcvendas/resources/CategoriaResource.java
+++ src/main/java/com/fcweb/fcvendas/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.fcweb.fcvendas.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fcweb.fcvendas.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String Listar()	{
-		return "REST esta funcionado"; 
+	public List<Categoria> Listar()	{
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista; 
 	}
 
 }
