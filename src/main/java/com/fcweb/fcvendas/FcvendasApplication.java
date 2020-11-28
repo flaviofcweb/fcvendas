f335cdf3522b5ebf74b45b87c1e062b83ea08036
flaviofcweb <flaviofcweb@gmail.com>
2020-11-28 15:57:52 -0300

    Projeto Criado

---
+++ src/main/java/com/fcweb/fcvendas/FcvendasApplication.java
@@ -0,0 +1,13 @@
+package com.fcweb.fcvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcvendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcweb/fcvendas/FcvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcweb.fcvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
